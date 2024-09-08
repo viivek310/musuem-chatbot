7761a73da30b9483a07e6e9e85046901e983b846
vivek <vivekvasant310@gmail.com>
2024-09-08 14:00:31 +0530

    change4

--- app/components/ChatBot.js
+++ app/components/ChatBot.js
@@ -5,6 +5,9 @@ function ChatBot() {
   const [chats, setChats] = useState([])
   const [input, setInput] = useState("")
   const [response, setResponse] = useState("")
+  const [query, setQuery] = useState("")
+  const [shows, setShows] = useState([])
+  const [selectedShow, setSelectedShow] = useState("")
   // console.log(input)
 
 
@@ -22,9 +25,23 @@ function ChatBot() {
       body: JSON.stringify({ input })
     })
     const data = await res.json()
-    console.log(data)
+    setResponse(data.response)
+    setQuery(data.query)
+    setShows(data.shows)
+    setChats(prev => setChats([...prev, { sent: false, message: response }]))
+    if(shows){
+      setQuery(prev=>prev+shows)
+    }
+    if(data.query){
+      setInput(prev=>prev+query)
+    }
 
 
+  }
+
+  const showClicked = (show)=>{
+    setChats(prev=>[...prev,{sent:false,message:show}])
+    setSelectedShow(show)
   }
   return (
     <aside className='border absolute h-[65svh] w-[22vw] bottom-13 right-10 bg-slate-200 rounded-lg rounded-br overflow-hidden py-3 px-5'>
@@ -36,9 +53,12 @@ function ChatBot() {
           <ChatMessage key={i} sent={chat.sent} message={chat.message} />
         ))}
 
+        {shows&&shows?.map((show,i)=>(
+          <div onClick={()=>showClicked(show)} className="rounded-md rounded-bl-none bg-slate-600 border border-white">{show}</div>
+        ))}
       </div>
       <form className="input border h-[5%] flex items-center border-black rounded-lg overflow-hidden " onSubmit={sendMessage}>
-        <input className='h-full w-[80%] border-none outline-none px-2' type="text" value={input} onChange={(e) => setInput(e.target.value)} />
+        <input className='h-full w-[80%] border-none outline-none px-2' type="text" value={input} onChange={(e) => setInput(prev=>prev+e.target.value)} />
         <div className="button flex justify-center w-[20%] items-center text-center ">
           <button className='text-white w-full bg-purple-500 border-none outline-none'  >Send</button>
         </div>
