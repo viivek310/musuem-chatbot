e1a6c61c2abf01ce309ca1c89910d0d880473030
vivek <vivekvasant310@gmail.com>
2024-09-08 13:12:52 +0530

    error solve

--- app/components/ChatBot.js
+++ app/components/ChatBot.js
@@ -8,42 +8,41 @@ function ChatBot() {
   // console.log(input)
 
 
-  const sendMessage = async(e)=>{
+  const sendMessage = async (e) => {
     e.preventDefault()
-    setChats(prev=>setChats([...prev,{sent:true,message:input}]))
+    setChats(prev => setChats([...prev, { sent: true, message: input }]))
     setInput("")
-    
-    const res =  await fetch("http://localhost:5000/chat",{
+
+    const res = await fetch("http://localhost:5000/chat", {
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
       },
       method: "POST",
-      body: JSON.stringify({input})
-  })
-    data = await res.json()
-    // if(data.response)
+      body: JSON.stringify({ input })
+    })
+    const data = await res.json()
     console.log(data)
 
-  
+
   }
   return (
     <aside className='border absolute h-[65svh] w-[22vw] bottom-13 right-10 bg-slate-200 rounded-lg rounded-br overflow-hidden py-3 px-5'>
-        <div className="chat h-[95%] overflow-auto  border-red-500 text-white ">
-            {/* <div className="chat self-end px-4 rounded-full float-right clear-both bg-slate-900">hiii  </div> */}
-            {/* <ChatMessage sent={true} message={"hiii"}/>
+      <div className="chat h-[95%] overflow-auto  border-red-500 text-white ">
+        {/* <div className="chat self-end px-4 rounded-full float-right clear-both bg-slate-900">hiii  </div> */}
+        {/* <ChatMessage sent={true} message={"hiii"}/>
             <ChatMessage sent={false} message={"hello"}/> */}
-           {chats?.map((chat,i)=>(
-              <ChatMessage key={i} sent={chat.sent} message={chat.message}/>
-           ))}
+        {chats?.map((chat, i) => (
+          <ChatMessage key={i} sent={chat.sent} message={chat.message} />
+        ))}
 
+      </div>
+      <form className="input border h-[5%] flex items-center border-black rounded-lg overflow-hidden " onSubmit={sendMessage}>
+        <input className='h-full w-[80%] border-none outline-none px-2' type="text" value={input} onChange={(e) => setInput(e.target.value)} />
+        <div className="button flex justify-center w-[20%] items-center text-center ">
+          <button className='text-white w-full bg-purple-500 border-none outline-none'  >Send</button>
         </div>
-        <form className="input border h-[5%] flex items-center border-black rounded-lg overflow-hidden " onSubmit={sendMessage}>
-            <input className='h-full w-[80%] border-none outline-none px-2' type="text" value={input} onChange={(e)=>setInput(e.target.value)} />
-            <div className="button flex justify-center w-[20%] items-center text-center ">
-                <button  className='text-white w-full bg-purple-500 border-none outline-none'  >Send</button>
-            </div>
-        </form>
+      </form>
     </aside>
   )
 }
