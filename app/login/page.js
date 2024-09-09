5bf25acca71befc299ee6cf0d9068dd8514b2d6d
vivek <vivekvasant310@gmail.com>
2024-09-09 15:39:09 +0530

    sign up

--- app/login/page.js
+++ app/login/page.js
@@ -3,16 +3,62 @@ import React, { useState } from 'react'
 
 function Page() {
     const [login, setLogin] = useState(true)
-    const [formdata,setFormdata] = useState("")
-  return (
-    <div className='flex justify-center items-center'>
-        <div className="login">
-            <div>
-                {/* <input type="text" name='username' value={username} onChange={(e)=>setFormdata(prev=>({...prev,[e.target.name]:e.target.value}))}/> */}
+    const [loginData, setLoginData] = useState({})
+    const [signUp, setSignUp] = useState({})
+
+    const handleform = async(e) => {
+        e.preventDefault()
+        const sendform = await fetch("http://localhost:5000/signup")
+        const data = await senform.json()
+        console.log(loginData)
+        setLoginData({})
+    }
+    const handleSignUp = (e) => {
+        e.preventDefault()
+        console.log(signUp)
+        setSignUp({})
+    }
+    return (
+        <div className='flex justify-center items-center min-h-[80svh]'>
+            {login ? <div className='border bg-slate-100 px-5 py-10 rounded-lg'>
+                <h1 className='text-center mb-3'>Log in</h1>
+                <form onSubmit={handleform} className='space-y-3'>
+                    <div>
+                        <input className='w-full border border-black rounded-lg px-3' type="text" name='username' value={loginData.username || ""} onChange={(e) => setLoginData(prev => ({ ...prev, [e.target.name]: e.target.value }))} />
+                    </div>
+                    <div>
+                        <input className='w-full border border-black rounded-lg px-3' type="password" name='password' value={loginData.password || ""} onChange={(e) => setLoginData(prev => ({ ...prev, [e.target.name]: e.target.value }))} />
+                    </div>
+                    <div className='button'>
+                        <button type='submit' className='border border-gray-800 w-full my-3 rounded-full bg-purple-500 text-white'>Log in </button>
+                    </div>
+                </form>
+                <div>
+                    Don't have an account? <button className='text-blue-500' onClick={()=>setLogin(false)}>Sign Up</button>
+                </div>
             </div>
+                :
+                <div className='border bg-slate-100 px-5 py-10 rounded-lg'>
+                    <h1 className='text-center mb-3'>Sign Up</h1>
+                    <form onSubmit={handleSignUp}  className='space-y-3'>
+                    <div>
+                        <input className='w-full border border-black rounded-lg px-3' type="text" name='username' value={signUp.username || ""} onChange={(e) => setSignUp(prev => ({ ...prev, [e.target.name]: e.target.value }))} />
+                    </div>
+                    <div>
+                        <input className='w-full border border-black rounded-lg px-3' type="password" name='password' value={signUp.password || ""} onChange={(e) => setSignUp(prev => ({ ...prev, [e.target.name]: e.target.value }))} />
+                    </div>
+                    <div className='button'>
+                        <button type='submit' className='border border-gray-800 w-full my-3 rounded-full bg-purple-500 text-white'>Register</button>
+                    </div>
+                </form>
+                <div>
+                    Already have an account? <button className='text-blue-500' onClick={()=>setLogin(true)}>Log in</button>
+                </div>
+                </div>
+            }
+
         </div>
-    </div>
-  )
+    )
 }
 
 export default Page
