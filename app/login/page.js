97d79dccd183a34829ed45f057001d16a2350b4b
vivek <vivekvasant310@gmail.com>
2024-09-09 15:54:19 +0530

    signup

--- app/login/page.js
+++ app/login/page.js
@@ -8,14 +8,30 @@ function Page() {
 
     const handleform = async(e) => {
         e.preventDefault()
-        const sendform = await fetch("http://localhost:5000/signup")
-        const data = await senform.json()
+        const sendform = await fetch("http://localhost:5000/signup",{
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+            },
+            method: "POST",
+            body: JSON.stringify()
+        })
+        const data = await sendform.json()
         console.log(loginData)
         setLoginData({})
     }
-    const handleSignUp = (e) => {
+    const handleSignUp = async(e) => {
         e.preventDefault()
-        console.log(signUp)
+        const sendform = await fetch("http://localhost:5000/signup",{
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+            },
+            method: "POST",
+            body: JSON.stringify(signUp)
+        })
+        const data = await sendform.json()
+        console.log(data)
         setSignUp({})
     }
     return (
@@ -24,7 +40,7 @@ function Page() {
                 <h1 className='text-center mb-3'>Log in</h1>
                 <form onSubmit={handleform} className='space-y-3'>
                     <div>
-                        <input className='w-full border border-black rounded-lg px-3' type="text" name='username' value={loginData.username || ""} onChange={(e) => setLoginData(prev => ({ ...prev, [e.target.name]: e.target.value }))} />
+                        <input className='w-full border border-black rounded-lg px-3' type="text" placeholder='username' name='username' value={loginData.username || ""} onChange={(e) => setLoginData(prev => ({ ...prev, [e.target.name]: e.target.value }))} />
                     </div>
                     <div>
                         <input className='w-full border border-black rounded-lg px-3' type="password" name='password' value={loginData.password || ""} onChange={(e) => setLoginData(prev => ({ ...prev, [e.target.name]: e.target.value }))} />
@@ -45,7 +61,13 @@ function Page() {
                         <input className='w-full border border-black rounded-lg px-3' type="text" name='username' value={signUp.username || ""} onChange={(e) => setSignUp(prev => ({ ...prev, [e.target.name]: e.target.value }))} />
                     </div>
                     <div>
+                        <input className='w-full border border-black rounded-lg px-3' type="mail" name='email' value={signUp.username || ""} onChange={(e) => setSignUp(prev => ({ ...prev, [e.target.name]: e.target.value }))} />
+                    </div>
+                    <div>
                         <input className='w-full border border-black rounded-lg px-3' type="password" name='password' value={signUp.password || ""} onChange={(e) => setSignUp(prev => ({ ...prev, [e.target.name]: e.target.value }))} />
+                    </div>
+                    <div>
+                        <input className='w-full border border-black rounded-lg px-3' type="password" name='confirm-password' value={signUp.password || ""} onChange={(e) => setSignUp(prev => ({ ...prev, [e.target.name]: e.target.value }))} />
                     </div>
                     <div className='button'>
                         <button type='submit' className='border border-gray-800 w-full my-3 rounded-full bg-purple-500 text-white'>Register</button>
