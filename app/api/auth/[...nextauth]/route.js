a36d6708c7a0c6bf3ec46727aa67e5e3fbf859fb
vivek <vivekvasant310@gmail.com>
2024-09-10 20:01:34 +0530

    signup1

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -20,31 +20,17 @@ export const authoptions = NextAuth({
             },
             async authorize(credentials, req) {
 
-                try {
-                    const res = await fetch("http://localhost:5000/login", {
-                        headers: {
-                            'Accept': 'application/json',
-                            'Content-Type': 'application/json'
-                        },
-                        method: "POST",
-                        body: JSON.stringify({
-                            username: credentials.username,
-                            password: credentials.password
-                        })
-                    })
-                    const data = await res.json()
-                    console.log("auth", data)
-                    // if (res.ok && data.user) {
-                    //     console.log(data.user)
-                    //     return data.user;
-                    // } else {
-                    //     throw new Error("error");
+                const user = { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
 
-                    // }
-                    return data.user
-
-                } catch (Error) {
-                    console.log(Error)
+                if (user) {
+                  // Any object returned will be saved in `user` property of the JWT
+                  return user
+                } else {
+                  // If you return null or false then the credentials will be rejected
+                  return null
+                  // You can also Reject this callback with an Error or with a URL:
+                  // throw new Error('error message') // Redirect to error page
+                  // throw '/path/to/redirect'        // Redirect to a URL
                 }
             }
         })
