658f51e9f03511b5759bbf4520a8550e246a696d
vivek <vivekvasant310@gmail.com>
2024-09-10 19:54:20 +0530

    login fix

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -6,51 +6,51 @@ import CredentialsProvider from "next-auth/providers/credentials";
 
 
 export const authoptions = NextAuth({
-  pages: {
-    signIn: '/login',
-  },
-  providers: [
-    
-    CredentialsProvider({
-      id: "credentials",
-      name: "Credentials",
-      credentials: {
-        username: { label: "Username", type: "text" },
-        password: { label: "Password", type: "password" }
-      },
-      async authorize(credentials, req) {
-       
-       try{
-        const res = await fetch("http://localhost:5000/login",{
-            headers: {
-              'Accept': 'application/json',
-              'Content-Type': 'application/json'
+    pages: {
+        signIn: '/login',
+    },
+    providers: [
+
+        CredentialsProvider({
+            id: "credentials",
+            name: "Credentials",
+            credentials: {
+                username: { label: "Username", type: "text" },
+                password: { label: "Password", type: "password" }
             },
-            method: "POST",
-            body: JSON.stringify({
-                username: credentials.username,
-                password: credentials.password
-              })
+            async authorize(credentials, req) {
+
+                try {
+                    const res = await fetch("http://localhost:5000/login", {
+                        headers: {
+                            'Accept': 'application/json',
+                            'Content-Type': 'application/json'
+                        },
+                        method: "POST",
+                        body: JSON.stringify({
+                            username: credentials.username,
+                            password: credentials.password
+                        })
+                    })
+                    const data = await res.json()
+                    console.log("auth", data)
+                    // if (res.ok && data.user) {
+                    //     console.log(data.user)
+                    //     return data.user;
+                    // } else {
+                    //     throw new Error("error");
+
+                    // }
+                    return data.user
+
+                } catch (Error) {
+                    console.log(Error)
+                }
+            }
         })
-        const data = await res.json()
-        console.log("auth",data)
-        if (res.ok && data.user) {
-            console.log(data.user)
-            return data.user; 
-          } else {
-            throw new Error("error");
-             
-          }
-
-
-       }catch(Error){
-            console.log(Error)
-       }
-      }
-    })
-  ],
-  secret: process.env.NEXTAUTH_SECRET,
-  url: process.env.NEXTAUTH_URL 
+    ],
+    secret: process.env.NEXTAUTH_SECRET,
+    url: process.env.NEXTAUTH_URL
 })
 
 export { authoptions as GET, authoptions as POST }
