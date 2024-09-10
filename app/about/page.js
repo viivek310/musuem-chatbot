b4d9eb3a7d6375fb9450aa12f65f3662692127b9
vivek <vivekvasant310@gmail.com>
2024-09-11 01:35:41 +0530

    frontend3

---
+++ app/about/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Page() {
+  return (
+    <div>
+      aboutklsajflksdjflkj
+    </div>
+  )
+}
+
+export default Page
--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -5,15 +5,15 @@ import NavLink from './NavLink'
 
 function Navbar() {
   return (
-    <header>
-      <nav className='border-2 shadow-md flex items-center justify-around bg-white fixed top-0 left-0 w-full'>
+    <header className='sticky top-0 z-50'>
+      <nav className='border-2 shadow-md flex items-center justify-around bg-white  w-full'>
         <div className="logo h-20  ">
             <Link href={"/"}>
               <Image className='h-full w-full object-cover' src={"/images/Untitled design.jpg"} width={1000} height={1000} alt='logo'/>
             </Link>
         </div>
         <ul className="navigation flex space-x-10 font-bold">
-            <NavLink link={"#"} page={"About"} />
+            <NavLink link={"/about"} page={"About"} />
             <NavLink link={"#"} page={"Shows"} />
             <NavLink link={"#"} page={"Booked Tickets"} />
             <NavLink link={"#"} page={"Guide"} />
