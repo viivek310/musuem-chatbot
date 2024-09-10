1de426e60fa5d973d6316a0b8052a556fe313cb2
vivek <vivekvasant310@gmail.com>
2024-09-10 17:29:08 +0530

    update

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -19,34 +19,24 @@ export const authoptions = NextAuth({
         password: { label: "Password", type: "password" }
       },
       async authorize(credentials, req) {
-        // try {
-        //   const user = await User.findOne({ username: credentials.username })
-        //   if (!user) {
-        //     throw new Error("Incorrect Username")
-        //   }
-        //   const isCorrect = await bcrypt.compare(credentials.password, user.password)
-        //   if (isCorrect) {
-        //     return user
-        //   } else {
-        //     throw new Error("Incorrect credentials")
-        //   }
-        // } catch (error) {
-        //   throw new Error(error)
-        // }
+       
         const res = await fetch("http://localhost:5000/login",{
             headers: {
               'Accept': 'application/json',
               'Content-Type': 'application/json'
             },
             method: "POST",
-            body: JSON.stringify({username:credentials.username})
+            body: JSON.stringify({
+                username: credentials.username,
+                password: credentials.password
+              })
         })
         const data = await res.json()
-        if(data.user){
-            return data.user
-        }else{
-            throw new Error(data.err)
-        }
+        if (res.ok && data.user) {
+            return data.user; 
+          } else {
+            return null; 
+          }
 
 
       }
