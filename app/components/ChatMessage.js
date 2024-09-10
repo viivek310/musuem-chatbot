ce52d59f94779ab61756db49dd42ffec858b02ea
vivek <vivekvasant310@gmail.com>
2024-09-11 03:45:47 +0530

    chatfix2

--- app/components/ChatMessage.js
+++ app/components/ChatMessage.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function ChatMessage({sent,message}) {
   return (
-    <div className={`chat px-4 py-1 rounded-lg ${sent?"rounded-br-none":"rounded-bl-none"} w-fit max-w-[70%] break-words ${sent?"float-right":"float-left"} clear-both ${sent?"bg-zinc-400":"bg-slate-200"} my-1 ${!sent&&"text-black"}`}>{message}</div>
+    <div className={`chat px-4 py-1 rounded-lg ${sent?"rounded-br-none":"rounded-bl-none"} w-fit max-w-[70%] break-words ${sent?"float-right":"float-left"} clear-both ${sent?"bg-zinc-700":"bg-slate-200"} my-1 ${!sent&&"text-black"}`}>{message}</div>
   )
 }
 
