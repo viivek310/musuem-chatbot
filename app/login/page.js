b7a34586443028cf8b3b16ce9a613b15fec2fc55
vivek <vivekvasant310@gmail.com>
2024-09-10 23:23:48 +0530

    test3

--- app/login/page.js
+++ app/login/page.js
@@ -45,7 +45,7 @@ function Page() {
             password: loginData.password,
             redirect: false
           })
-          console.log(res.user,"user")
+          console.log(res,"user")
         setLoginData({})
     }
     const handleSignUp = async (e) => {
