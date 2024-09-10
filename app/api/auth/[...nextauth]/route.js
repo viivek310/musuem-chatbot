0db915234706343ef0dd3593dceaf1c166ad3c14
vivek <vivekvasant310@gmail.com>
2024-09-10 20:15:29 +0530

    login4

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -25,7 +25,7 @@ export const authoptions = NextAuth({
                 // if (user) {
                 //   // Any object returned will be saved in `user` property of the JWT
                 console.log(user)
-                  return user
+                  return "hello"
                 // } else {
                 //   // If you return null or false then the credentials will be rejected
                 //   return null
