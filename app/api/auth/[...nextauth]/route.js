e95f9ab039b398d3b34477f5ea8822f31ca1c8e2
vivek <vivekvasant310@gmail.com>
2024-09-10 22:17:46 +0530

    login

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -10,7 +10,6 @@ export const authoptions = NextAuth({
         signIn: '/login',
     },
     providers: [
-
         CredentialsProvider({
             id: "credentials",
             name: "Credentials",
@@ -19,48 +18,27 @@ export const authoptions = NextAuth({
                 password: { label: "Password", type: "password" }
             },
             async authorize(credentials, req) {
-
-                try{
-                    const res = fetch("http://localhost:5000/login",{
-                        headers: {
-                          'Accept': 'application/json',
-                          'Content-Type': 'application/json'
-                        },
-                        method: "POST",
-                        body: JSON.stringify({res:"hello"})
-                    })
-    
-                    const data = await res.json()
-                    return data.user
-                }catch(error){
-                    console.log(error)
-                    throw new Error("erororor")
-                }
-                // console.log(data)
-
-                // const user = { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
-
-                // // if (user) {
-                // //   // Any object returned will be saved in `user` property of the JWT
-                //   return user
-                // } else {
-                //   // If you return null or false then the credentials will be rejected
-                //   return null
-                //   // You can also Reject this callback with an Error or with a URL:
-                //   // throw new Error('error message') // Redirect to error page
-                //   // throw '/path/to/redirect'        // Redirect to a URL
-                // }
-
+                const res = fetch("http://localhost:5000/login", {
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    },
+                    method: "POST",
+                    body: JSON.stringify({ a: 1, b: 2 })
+                })
+                const data = await res.json()
+                console.log(data)
+                return data
             }
         })
     ],
     callbacks: {
         async signIn({ user, account, profile, email, credentials }) {
-          if (account?.provider == "credentials") {
-            return true
-          }
+            if (account?.provider == "credentials") {
+                return true
+            }
         },
-      },
+    },
 })
 
 export { authoptions as GET, authoptions as POST }
