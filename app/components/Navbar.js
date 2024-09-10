2d3842c585e305079eb970d8414d5dc922a38246
vivek <vivekvasant310@gmail.com>
2024-09-11 01:23:37 +0530

    frontend2

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -6,7 +6,7 @@ import NavLink from './NavLink'
 function Navbar() {
   return (
     <header>
-      <nav className='border-2 shadow-md flex items-center justify-around bg-white'>
+      <nav className='border-2 shadow-md flex items-center justify-around bg-white fixed top-0 left-0 w-full'>
         <div className="logo h-20  ">
             <Link href={"/"}>
               <Image className='h-full w-full object-cover' src={"/images/Untitled design.jpg"} width={1000} height={1000} alt='logo'/>
