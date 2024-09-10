cb1cb599a93a57c746772d3068477ac0db8434f6
vivek <vivekvasant310@gmail.com>
2024-09-10 20:04:06 +0530

    signup3

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -22,16 +22,16 @@ export const authoptions = NextAuth({
 
                 const user = { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
 
-                if (user) {
-                  // Any object returned will be saved in `user` property of the JWT
+                // if (user) {
+                //   // Any object returned will be saved in `user` property of the JWT
                   return user
-                } else {
-                  // If you return null or false then the credentials will be rejected
-                  return null
-                  // You can also Reject this callback with an Error or with a URL:
-                  // throw new Error('error message') // Redirect to error page
-                  // throw '/path/to/redirect'        // Redirect to a URL
-                }
+                // } else {
+                //   // If you return null or false then the credentials will be rejected
+                //   return null
+                //   // You can also Reject this callback with an Error or with a URL:
+                //   // throw new Error('error message') // Redirect to error page
+                //   // throw '/path/to/redirect'        // Redirect to a URL
+                // }
             }
         })
     ],
