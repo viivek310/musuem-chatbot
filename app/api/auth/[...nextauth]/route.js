9f67f10ec777ee7c063bed37290ea57311aeec4b
vivek <vivekvasant310@gmail.com>
2024-09-11 06:39:15 +0530

    login final2

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -6,26 +6,7 @@ export const authOptions = {
     CredentialsProvider({
       async authorize(credentials) {
         // Call your Flask backend to authenticate the user
-        const res = await fetch('http://localhost:5000/login', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            username: credentials.username,
-            password: credentials.password,
-          }),
-        });
-
-        const user = await res.json();
-
-        if (res.ok && user) {
-          // Return the user object if authentication is successful
-          return user;
-        } else {
-          // Return null if authentication fails
-          return null;
-        }
+        return credentials
       },
     }),
   ],
