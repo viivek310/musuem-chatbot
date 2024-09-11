88d358e0cf524b5a54f3f7a5a818b69a698a1923
vivek <vivekvasant310@gmail.com>
2024-09-11 06:43:20 +0530

    login final 3

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,21 +1,40 @@
-import NextAuth from 'next-auth';
-import CredentialsProvider from 'next-auth/providers/credentials';
+import NextAuth from 'next-auth'
+import GitHubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+import connectDB from '@/connection';
+import User from '@/models/user'
+import bcrypt from "bcrypt"
 
-export const authOptions = {
+
+
+
+
+export const authoptions = NextAuth({
+  pages: {
+    signIn: '/login',
+  },
   providers: [
+    // OAuth authentication providers...
     CredentialsProvider({
-      async authorize(credentials) {
-        // Call your Flask backend to authenticate the user
-        return credentials
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text" },
+        password: { label: "Password", type: "password" }
       },
-    }),
+      async authorize(credentials, req) {
+        return credentials
+      }
+    })
   ],
   callbacks: {
-    async session({ session, token }) {
-      session.user = token;
-      return session;
+    async signIn({ user, account, profile, email, credentials }) {
+      if (account?.provider == "credentials") {
+        return true
+      }
     },
   },
-};
+})
 
-export default NextAuth(authOptions);
+export { authoptions as GET, authoptions as POST }
