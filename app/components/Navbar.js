0f57fc9f8929c7abe23f6d44ba4de8dfea419f2a
vivek <vivekvasant310@gmail.com>
2024-09-11 06:06:50 +0530

    login push

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,7 +1,7 @@
 "use client "
 import Image from 'next/image'
 import Link from 'next/link'
-import React, { useEffect } from 'react'
+import React from 'react'
 import NavLink from './NavLink'
 
 function Navbar() {
