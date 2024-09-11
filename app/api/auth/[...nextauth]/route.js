ae213ca6a2678159e9b470ba77a71d2329e4b60f
vivek <vivekvasant310@gmail.com>
2024-09-11 06:55:18 +0530

    login final*3

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -20,7 +20,7 @@ export const authoptions = NextAuth({
         password: { label: "Password", type: "password" }
       },
       async authorize(credentials, req) {
-        return {username: credentials.username,password: credentials.password, email: credentials.emai}
+        return {username: credentials.username,password: credentials.password, email: credentials.email}
       }
     })
   ],
